e50edfe00cdb9b4ac70694868420ed83fa5dab62
sarmen-t <sarmenteroganesyan@gmail.com>
2023-05-09 19:58:58 -0700

    Working subtract, almost working negative

--- calculator/script.js
+++ calculator/script.js
@@ -6,20 +6,33 @@ const num = document.querySelectorAll('#num')
 const mainNum = document.getElementById('mainNum')
 const hnl = document.getElementById('hn')
 
+const clear = document.getElementById('clear')
+clear.addEventListener('click', () => {
+    mainNum.innerHTML = 0
+    func = 'none'
+    hold_num = 0
+    console.log('holdnum ' + hold_num)
+    console.clear()
+})
 
-for (let i = 0; i < num.length; i++) {
-    num[i].addEventListener('click', function (num) {
-        if (func == 'add') {
-            updateNum2(num)
-        }
-        else if (func == 'eq'){ // probably have to change this for other operators
-            updateNum3(num)
-        }
-        else {
-            updateNum(num)
-        }
-    })
-}
+const back = document.getElementById('back')
+back.addEventListener('click', () => {
+    if (mainNum.innerHTML == 0){}
+    else if (mainNum.innerHTML < 10){
+        mainNum.innerHTML = 0
+    }
+    else {
+        let hold = mainNum.innerHTML
+        hold = hold.slice(0, -1)
+        mainNum.innerHTML = hold
+    }
+})
+
+const plusMinus = document.getElementById('plusmin') // only works with first number
+// not during operations
+plusMinus.addEventListener('click', () => {
+    mainNum.innerHTML *= -1
+})
 
 function updateNum (num) {
     currNum = mainNum.innerHTML
@@ -59,27 +72,20 @@ function updateNum3 (num) {
     }
 }
 
-const clear = document.getElementById('clear')
-clear.addEventListener('click', () => {
-    mainNum.innerHTML = 0
-    func = 'none'
-    hold_num = 0
-    hnl.innerHTML = `hold num: 0`
-    console.clear()
-})
-
-const back = document.getElementById('back')
-back.addEventListener('click', () => {
-    if (mainNum.innerHTML == 0){}
-    else if (mainNum.innerHTML < 10){
-        mainNum.innerHTML = 0
-    }
-    else {
-        let hold = mainNum.innerHTML
-        hold = hold.slice(0, -1)
-        mainNum.innerHTML = hold
-    }
-})
+for (let i = 0; i < num.length; i++) {
+    num[i].addEventListener('click', function (num) {
+        if (func == 'add' || func == 'sub') {
+            updateNum2(num)
+        }
+        else if (func == 'eqadd' || func == 'eqsub'){ 
+        // probably have to change this for other operators
+            updateNum3(num)
+        }
+        else {
+            updateNum(num)
+        }
+    })
+}
 
 const add = document.getElementById('add')
 add.addEventListener('click', () => {
@@ -87,14 +93,32 @@ add.addEventListener('click', () => {
         currNum = 0
         addFunc()
         hold_num = mainNum.innerHTML
-        hnl.innerHTML = `hold num: ${hold_num}`
+        console.log('holdnum ' + hold_num)
     }
     else {
         currNum = 0
         func = 'add'
         hold_num = mainNum.innerHTML
-        hnl.innerHTML = `hold num: ${hold_num}`
+        console.log('holdnum ' + hold_num)
         mainNum.innerHTML += ' + '
+        //mainNum.innerHTML = 0
+    }
+})
+
+const sub = document.getElementById('sub')
+sub.addEventListener('click', () => {
+    if (func == 'sub') {
+        currNum = 0
+        subFunc()
+        hold_num = mainNum.innerHTML
+        console.log('holdnum ' + hold_num)
+    }
+    else {
+        currNum = 0
+        func = 'sub'
+        hold_num = mainNum.innerHTML
+        console.log('holdnum ' + hold_num)
+        mainNum.innerHTML += ' - '
         //mainNum.innerHTML = 0
     }
 })
@@ -103,11 +127,20 @@ const equal = document.getElementById('equal')
 equal.addEventListener('click', () => {
     if (func == 'add') {
         addFunc()
-        func = 'eq'
+        func = 'eqadd'
         currNum = 0
     }
-    else if (func == 'eq'){ //same for this
+    else if (func == 'sub') {
+        subFunc()
+        func = 'eqsub'
+        currNum = 0
+    }
+    else if (func == 'eqadd'){ //same for this
         addFunc()
+        currNum = 0
+    }
+    else if (func == 'eqsub'){ //same for this
+        subFunc()
         currNum = 0
     }
 })
@@ -120,4 +153,14 @@ function addFunc () {
         mainNum.innerHTML = parseInt(mainNum.innerHTML) + parseInt(hold_num2)
     }
 }
+
+function subFunc () {
+    if (func == 'sub') {
+        mainNum.innerHTML =  parseInt(hold_num) - parseInt(mainNum.innerHTML)
+    }
+    else {
+        mainNum.innerHTML = parseInt(mainNum.innerHTML) - parseInt(hold_num2)
+    }
+}
+
 
