242a04d82ee5b5e1f201af2f412e36c00bcebaac
sarmen-t <sarmenteroganesyan@gmail.com>
2023-05-09 18:14:26 -0700

    Working addition function

--- calculator/script.js
+++ calculator/script.js
@@ -10,10 +10,10 @@ const hnl = document.getElementById('hn')
 for (let i = 0; i < num.length; i++) {
     num[i].addEventListener('click', function (num) {
         if (func == 'add') {
-            mainNum.innerHTML = num.target.innerHTML
+            updateNum2(num)
         }
-        else if (func == 'eq'){
-            mainNum.innerHTML = num.target.innerHTML
+        else if (func == 'eq'){ // probably have to change this for other operators
+            updateNum3(num)
         }
         else {
             updateNum(num)
@@ -33,10 +33,38 @@ function updateNum (num) {
     }
 }
 
+function updateNum2 (num) {
+    if (currNum == 0) {
+        mainNum.innerHTML = num.target.innerHTML
+        currNum = mainNum.innerHTML
+        hold_num2 = mainNum.innerHTML
+    }
+    else {
+        mainNum.innerHTML = currNum + num.target.innerHTML
+        currNum = mainNum.innerHTML
+        hold_num2 = mainNum.innerHTML
+    }
+}
+
+function updateNum3 (num) {
+    if (currNum == 0) {
+        mainNum.innerHTML = num.target.innerHTML
+        currNum = mainNum.innerHTML
+        hold_num2 = mainNum.innerHTML
+    }
+    else {
+        currNum = mainNum.innerHTML
+        mainNum.innerHTML = currNum + num.target.innerHTML
+        hold_num2 = mainNum.innerHTML
+    }
+}
+
 const clear = document.getElementById('clear')
 clear.addEventListener('click', () => {
     mainNum.innerHTML = 0
     func = 'none'
+    hold_num = 0
+    hnl.innerHTML = `hold num: 0`
     console.clear()
 })
 
@@ -56,15 +84,17 @@ back.addEventListener('click', () => {
 const add = document.getElementById('add')
 add.addEventListener('click', () => {
     if (func == 'add') {
+        currNum = 0
         addFunc()
         hold_num = mainNum.innerHTML
         hnl.innerHTML = `hold num: ${hold_num}`
     }
     else {
+        currNum = 0
         func = 'add'
         hold_num = mainNum.innerHTML
         hnl.innerHTML = `hold num: ${hold_num}`
-        mainNum.innerHTML += ' +'
+        mainNum.innerHTML += ' + '
         //mainNum.innerHTML = 0
     }
 })
@@ -74,17 +104,16 @@ equal.addEventListener('click', () => {
     if (func == 'add') {
         addFunc()
         func = 'eq'
-        //hold_num = mainNum.innerHTML
+        currNum = 0
     }
-    else if (func == 'eq'){
-        console.log('test')
+    else if (func == 'eq'){ //same for this
         addFunc()
+        currNum = 0
     }
 })
 
 function addFunc () {
     if (func == 'add') {
-        console.log('oldnum ' + mainNum.innerHTML + ' hn ' + hold_num)
         mainNum.innerHTML = parseInt(mainNum.innerHTML) + parseInt(hold_num)
     }
     else {
